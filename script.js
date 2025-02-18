b24cdf1c522e33bcd857285e47b90aab83357cc8
Sergei Bondar <sb49075954@gmail.com>
2025-02-17 23:43:03 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function createSpark() {
     // Posición inicial aleatoria dentro del contenedor
     const randomX = Math.random() * 160 - 100; // De -30px a 30px
     const randomDelay = Math.random() * 0.1; // Hasta 1.5s de retraso
-    const randomDuration = Math.random() * 5.5 + 3.8; // Duración entre 0.8s y 1.3s
+    const randomDuration = Math.random() * 7.5 + 3.8; // Duración entre 0.8s y 1.3s
 
     spark.style.left = `${Math.random() * 100}%`; // Posición horizontal aleatoria
     spark.style.setProperty("--random-x", `${randomX}px`); // Movimiento lateral aleatorio
